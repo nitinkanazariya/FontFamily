d27f5d3cb671b4df75debf65e8acf22096326d31
Nitin Kanazariya <nitinkanazariya61506@gmail.com>
2024-01-29 16:07:34 +0530

    first

---
+++ App.js
@@ -0,0 +1,34 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+      <Text
+        style={{fontFamily: 'Agbalumo-Regular', fontSize: 30, color: 'black'}}>
+        App
+      </Text>
+      <Text style={{fontFamily: 'Gugi-Regular', fontSize: 30, color: 'black'}}>
+        App
+      </Text>
+      <Text
+        style={{fontFamily: 'Nosifer-Regular', fontSize: 30, color: 'black'}}>
+        App
+      </Text>
+      <Text
+        style={{fontFamily: 'Pacifico-Regular', fontSize: 30, color: 'black'}}>
+        App
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'Sacramento-Regular',
+          fontSize: 30,
+          color: 'black',
+        }}>
+        App
+      </Text>
+    </View>
+  );
+};
+
+export default App;
